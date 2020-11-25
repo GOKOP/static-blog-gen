2541f164a355b8b1c4dbe2cb5666c4bdcb45cb70
GOKOP <igorbugajski@protonmail.com>
2020-11-25 17:21:15 +0100

    simplified generate-roll.sh

--- generate-roll.sh
+++ generate-roll.sh
@@ -12,15 +12,10 @@ for file in $(ls posts-md); do
 	if [ -z $prev_year ] || [ $year != $prev_year ]; then
 		year_n=$(echo $year_n+1 | bc)
 		echo "$year_n $year" >> temp/year_list
-
-		cat html/roll/post-head.html | sed 's|<!-- date !-->|'$date'|' > temp/years/$year
-		pandoc posts-md/$file -f markdown -t html >> temp/years/$year
-		cat html/common/back-to-top.html >> temp/years/$year
-	else
-		cat html/roll/post-head.html | sed 's|<!-- date !-->|'$date'|' >> temp/years/$year
-		pandoc posts-md/$file -f markdown -t html >> temp/years/$year
-		cat html/common/back-to-top.html >> temp/years/$year
 	fi
+	cat html/roll/post-head.html | sed 's|<!-- date !-->|'$date'|' >> temp/years/$year
+	pandoc posts-md/$file -f markdown -t html >> temp/years/$year
+	cat html/common/back-to-top.html >> temp/years/$year
 	
 	prev_year=$year
 done
