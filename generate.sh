7bc1c44cc0bb8cf7486f9abce0d01fbcd71cc9d4
GOKOP <igorbugajski@protonmail.com>
2020-11-25 18:42:12 +0100

    symlinks to the latest year

--- generate.sh
+++ generate.sh
@@ -6,3 +6,7 @@ script/generate-index.sh
 script/generate-roll.sh
 
 cp -r css site/
+
+last_year=$(ls site/index | tail -n 1)
+ln -s $last_year site/index/index.html
+ln -s $last_year site/roll/index.html
