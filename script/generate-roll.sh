3fc669dff8e20234bed9a5a51879cdbd381bb8e8
GOKOP <igorbugajski@protonmail.com>
2020-12-03 22:59:11 +0100

    using generated post dates in generate-roll.sh

--- script/generate-roll.sh
+++ script/generate-roll.sh
@@ -5,7 +5,8 @@
 year_n=0
 
 for file in $(ls -r posts-md); do
-	date=$(echo $file | sed 's|_.*||; s|\.md||')
+	date=$(head -n1 temp/postdata/$file)
+	[ $(wc -l < temp/postdata/$file) = 2 ] && edit_date=$(tail -n1 temp/postdata/$file)
 	year=$(echo $date | sed 's|-.*||')
 	html_file=$(echo $file | sed 's|\.md|.html|')
 
@@ -13,8 +14,15 @@ for file in $(ls -r posts-md); do
 		year_n=$(echo $year_n+1 | bc)
 		echo "$year_n $year" >> temp/year_list
 	fi
-	sed 's|<!-- date !-->|'$date'|' html/roll/post-head.html > temp/top
-	sed 's|<!-- post !-->|'$html_file'|' temp/top >> temp/years/$year
+	sed 's|<!-- date !-->|'$date'|' html/roll/post-head.html > temp/top0
+
+	if [ -z $edit_date ]; then
+		sed 's|.*<!-- edit date !-->.*||' temp/top0 > temp/top1
+	else
+		sed 's|<!-- edit date !-->|'$edit_date'|' temp/top0 > temp/top1
+	fi
+
+	sed 's|<!-- post !-->|'$html_file'|' temp/top1 >> temp/years/$year
 	cat temp/posts/$html_file >> temp/years/$year
 	cat html/common/back-to-top.html >> temp/years/$year
 	
